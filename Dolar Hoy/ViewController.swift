0cbd9463c3f1eb1fea3f7d9ad32d364d5a207ed8
arifads <arifads@gmail.com>
2015-04-25 15:23:23 -0430

    Initial Commit

---
+++ Dolar Hoy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dolar Hoy
+//
+//  Created by Arif De Sousa on 4/25/15.
+//  Copyright (c) 2015 Arif De Sousa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
